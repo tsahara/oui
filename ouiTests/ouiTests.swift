0a2d21c85a377a93cc733f598cef6840b0ae1d01
Tomoyuki Sahara <sahara@caddr.net>
2015-12-12 21:55:45 +0900

    Initial Commit

---
+++ ouiTests/ouiTests.swift
@@ -0,0 +1,36 @@
+//
+//  ouiTests.swift
+//  ouiTests
+//
+//  Created by Tomoyuki Sahara on 12/12/15.
+//  Copyright © 2015 Tomoyuki Sahara. All rights reserved.
+//
+
+import XCTest
+@testable import oui
+
+class ouiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
